56e8e8a0b052396864ed42ad4a10633f578e2c8f
L.Z.Che <FadingLight9291117@126.com>
2020-03-20 10:05:10 +0800

    home page

---
+++ src/plugins/element.js
@@ -0,0 +1,9 @@
+import Vue from "vue";
+import { Button, Menu, Submenu, MenuItem, MenuItemGroup } from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+
+Vue.use(Button);
+Vue.use(Menu);
+Vue.use(Submenu);
+Vue.use(MenuItem);
+Vue.use(MenuItemGroup)
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="home">
+    <header>
+      <el-menu
+        :default-active="activeIndex"
+        mode="horizontal"
+        @select="handleSelect"
+        class="menu"
+      >
+        <el-menu-item index="首页">首页</el-menu-item>
+        <el-submenu index="工作">
+          <template slot="title">工作</template>
+          <el-menu-item index="选项1">选项1</el-menu-item>
+          <el-submenu index="选项2">
+            <template slot="title">选项2</template>
+            <el-menu-item index="选项3">选项3</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+      </el-menu>
+    </header>
+    <div class="bg">
+      <h1>跆拳道</h1>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
 
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
+@Component
+export default class Home extends Vue {
+  activeIndex = "1";
+
+  handleSelect(key: string, keyPath: Array<string>) {
+    console.log(key, keyPath);
   }
-};
+}
 </script>
+
+<style scoped>
+#home {
+  text-align: left;
+}
+.menu {
+  text-align: left;
+  padding-left: 30px;
+}
+.bg {
+  height: 300px;
+  background-color: #282c34;
+  color: #61dafb;
+  text-align: center;
+  padding-top: 80px;
+  letter-spacing: 0.01em;
+  font-weight: 400px;
+  font-size: 60px;
+  line-height: 1.15;
+}
+</style>
